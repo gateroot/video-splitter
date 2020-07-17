35ad4bfd49e365a6521c5990e9f5b65e4dd5c2e8
sekine <sekine@cloud-ace.jp>
2020-07-17 22:50:02 +0900

    add copy option

--- infrastructure/encoder/encode.go
+++ infrastructure/encoder/encode.go
@@ -1,9 +1,9 @@
 package encoder
 
 import (
+	"github.com/gateroot/video-splitter/domain"
 	"os/exec"
 	"strconv"
-	"github.com/gateroot/video-splitter/domain"
 )
 
 type FFmpegEncoder struct {
@@ -12,7 +12,7 @@ type FFmpegEncoder struct {
 func (f FFmpegEncoder) Encode(input string, seq domain.Sequence, output string) error {
 	start := strconv.FormatFloat(seq.Start, 'f', -1, 64)
 	end := strconv.FormatFloat(seq.End, 'f', -1, 64)
-	cmd := exec.Command("ffmpeg", "-i", input, "-ss", start, "-to", end, output)
+	cmd := exec.Command("ffmpeg", "-i", input, "-ss", start, "-to", end, "-vcodec copy", "-acodec copy", output)
 
 	if err := cmd.Run(); err != nil {
 		return err
